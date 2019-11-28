061c4db7e680f52bc7ae8e47f7904277b7fa41ec
Tony <54376590+Antonio-Zegarra-Becerra@users.noreply.github.com>
2019-11-28 07:38:55 -0500

    Create tipos.h

---
+++ tipos.h
@@ -0,0 +1,10 @@
+#ifndef PROYECTO-FINAL-STREETLAB_TIPOS_H
+#define PROYECTO-FINAL-STREETLAB_TIPOS_H
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+typedef int Entero;
+typedef string Palabra;
+#endif PROYECTO-FINAL-STREETLAB_TIPOS_H
